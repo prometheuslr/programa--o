d1b164f018fa110f8400b0308a83fee99a696cf6
prometheuslr <prometheus.inves@gmail.com>
2023-11-19 17:37:36 -0300

    finished

--- web/project01/script.js
+++ web/project01/script.js
@@ -1,7 +1,10 @@
 const wrapper = document.querySelector('.wrapper');
 const registerLink = document.querySelector('.register-link');
-const logibLink = document.querySelector('.login-link');
+const loginLink = document.querySelector('.login-link');
 
 registerLink.onclick = () =>{
     wrapper.classList.add('active');
+}
+loginLink.onclick = () =>{
+    wrapper.classList.remove('active');
 }
